812c1269194af82a0d1f93bcafca32d10ce274ac
takuma-hmng8 <t.hashimoto@funtech.inc>
2025-02-10 13:48:15 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       rollupOptions: {
          output: {
             dir: "./build",
+            format: "umd",
             name: "Handshake",
          },
       },
